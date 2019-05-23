1bcc6b20a39883b15e8ecba0766b7014845e792f
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-05-23 19:53:33 +0200

    first commit

---
+++ binary-exercise.js
@@ -0,0 +1,6 @@
+// Create a random number between 0 to 15
+//for each number assign the proper binary in a switch statement and print it
+// 0 = 0000
+// 2 = 0010
+// 3 = 0011
+// Go tiger!NO CHEATING!
